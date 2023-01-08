42d24360e4ddbf444bdd42a95138d12a3d7d1bd7
HariniDharanikota <120095820+HariniDharanikota@users.noreply.github.com>
2023-01-08 22:38:17 +0530

    #include<stdio.h> int main() {     int n;     char c1,c2;     scanf("%d",&n);     for(c1=65;c1<=(64+n);c1++)     {         for(c2=65;c2<=(64+n);c2++)         printf("%c ",c1);         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,j;
+    int n;
+    char c1,c2;
     scanf("%d",&n);
-    for(i=n;i>=1;i--)
+    for(c1=65;c1<=(64+n);c1++)
     {
-        for(j=n;j>=1;j--)
-        printf("%d ",j);
+        for(c2=65;c2<=(64+n);c2++)
+        printf("%c ",c1);
         printf("
 ");
     }
